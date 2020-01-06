733684ba17e4c72d9c6d5fc9cd71df11a416dbe3
ChanceDurr <chancedurr@gmail.com>
2020-01-06 10:35:02 -0800

    Django Startup

--- beastmode/settings.py
+++ beastmode/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')zx4o)v+@=vv=m#4+n*2e3aj%g46$xz06lmmrd9e(^(t$sqolw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
