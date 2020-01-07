efb033da964a93495c43dcaf886e2e3be7a7edf0
ChanceDurr <chancedurr@gmail.com>
2020-01-07 08:45:28 -0800

    testing db

--- charlie/create_test.py
+++ charlie/create_test.py
@@ -1,5 +1,7 @@
 from models import Room
 from django.contrib.auth.models import User
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beastmode.settings')
+
 random_room = Room(title='A random room', description='Very randomness')
 random_room.save()
